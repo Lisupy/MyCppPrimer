9749f84be5b95be41b3b0cfb4a86e4fcad0b404e
Lisupy <zhong550413470@gmail.com>
2017-10-14 22:32:21 +0800

    实现第13章22题HasPtr的拷贝复制和赋值操作符

---
+++ Chapter13/Problem22/HasPtr.h
@@ -0,0 +1,28 @@
+#ifndef HASPTR_H
+#define HASPTR_H
+
+#include <string>
+
+class HasPtr{
+public:
+    HasPtr(): cnt(0), p(nullptr) {}
+    HasPtr(const HasPtr& other): cnt(other.cnt), p(new std::string(*other.p)) {}
+
+    HasPtr& operator=(const HasPtr& rhs){
+        auto new_p = new std::string(*rhs.p);
+        delete p;
+        p = new_p;
+        cnt = rhs.cnt;
+        return *this;
+    }
+
+    ~HasPtr(){
+        delete p;
+    }
+
+private:
+    int cnt;
+    std::string *p;
+};
+
+#endif
